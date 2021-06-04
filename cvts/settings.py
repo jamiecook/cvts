a9263fd91a235bf86cfa9f638c05010e8350f240
Simon Knapp <simon.knapp@csiro.au>
2021-06-04 17:11:28 +1000

    Allow setting directory structure from environment vars.
    - Default work directory is ~/.cvts.
    - Check that raw data folder is present.

--- cvts/settings.py
+++ cvts/settings.py
@@ -1,19 +1,32 @@
 import os
 
-WORK_PATH       = os.path.sep + os.path.join('datasets', 'work', 'lw-cvts', 'work')
-BOUNDARIES_PATH = os.path.join(WORK_PATH, 'boundaries')
-RAW_PATH        = os.path.join(WORK_PATH, 'raw')
-CONFIG_PATH     = os.path.join(WORK_PATH, 'config')
-OUT_PATH        = os.path.join(WORK_PATH, 'output')
-SEQ_PATH        = os.path.join(OUT_PATH,  'seq')
-MM_PATH         = os.path.join(OUT_PATH,  'mm')
-STOP_PATH       = os.path.join(OUT_PATH,  'stop')
-SRC_DEST_PATH   = os.path.join(OUT_PATH,  'src_dest')
-SPEED_PATH      = os.path.join(OUT_PATH,  'speed')
+DEBUG = False
+
+def _get_path(var, must_exist=False):
+    path = os.environ.get(
+        'CVTS_{}_PATH'.format(var.upper()),
+        os.path.join(WORK_PATH, var))
+
+    if must_exist and not os.path.isdir(path):
+        raise Exception('raw data directory ({}) does not exist'.format(path))
+
+    return path
+
+#: Default root directory
+WORK_PATH       = os.environ.get(
+    'CVTS_WORK_PATH', os.path.join(os.path.expanduser("~"), '.cvts'))
+RAW_PATH        = _get_path('raw', True)
+BOUNDARIES_PATH = _get_path('boundaries')
+CONFIG_PATH     = _get_path('config')
+OUT_PATH        = _get_path('output')
+
+SEQ_PATH        = os.path.join(OUT_PATH, 'seq')
+MM_PATH         = os.path.join(OUT_PATH, 'mm')
+STOP_PATH       = os.path.join(OUT_PATH, 'stop')
+SRC_DEST_PATH   = os.path.join(OUT_PATH, 'src_dest')
+SPEED_PATH      = os.path.join(OUT_PATH, 'speed')
 
 VALHALLA_CONFIG_FILE = os.path.join(CONFIG_PATH, 'valhalla.json')
-
-DEBUG = False
 
 for p in (CONFIG_PATH, OUT_PATH, SEQ_PATH, MM_PATH, STOP_PATH, SRC_DEST_PATH, SPEED_PATH):
     if not os.path.exists(p):
@@ -23,8 +36,8 @@ if __name__ == '__main__':
     # this won't work on windows
     print(';'.join('export CVTS_{}={}'.format(v, eval(v)) for v in (
         'WORK_PATH',
-        'BOUNDARIES_PATH',
         'RAW_PATH',
+        'BOUNDARIES_PATH',
         'CONFIG_PATH',
         'OUT_PATH',
         'SEQ_PATH',
